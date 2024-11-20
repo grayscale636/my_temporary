0074443e3737db84d5237561b688355220032ff3
gerypangestu00 <67625344+grayscale636@users.noreply.github.com>
2024-11-20 16:22:21 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 process main(void)
 {
     pid32   shpid;      /* Shell process ID */
-
+    
     printf("\n\n");
-
+    
     /* Create a local file system on the RAM disk */
     lfscreate(RAM0, 40, 20480);
-
+    
     /* Run the Xinu shell */
     recvclr();
     resume(shpid = create(shell, 8192, 50, "shell", 1, CONSOLE));
-
+    
     /* Wait for shell to exit and recreate it */
     while (TRUE) {
         if (receive() == shpid) {
